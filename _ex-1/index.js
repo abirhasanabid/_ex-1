e1b5d5076803d20e1317a03299404419633aa82e
projects <abid.7.tm@gmail.com>
2024-08-10 16:55:36 +0600

    js library exercise

---
+++ _ex-1/index.js
@@ -0,0 +1,9 @@
+var firstName = prompt("First Name :");
+var lastName = prompt("Last Name :");
+var fullName = firstName + lastName;
+var fullNameL = fullName.length
+//   var fullName2 = firstName.concat(lastName);
+document.write("Full Name : " + fullName);
+document.write(" Full Name Length : " + fullNameL);
+document.write(fullName.toUpperCase());
+document.write(fullName.charAt(2));
